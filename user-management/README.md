8856a763bfe6e007948c9c839ca46a0a26d53add
Jeisse Rocha <x21129924@student.ncirl.ie>
2022-03-27 10:38:52 +0100

    included signin functionality

--- user-management/README.md
+++ user-management/README.md
@@ -11,6 +11,13 @@ authorName: 'Serverless, inc.'
 authorAvatar: 'https://avatars1.githubusercontent.com/u/13742415?s=200&v=4'
 -->
 
+
+# Dependencies for this API
+```bash
+pip install boto3
+pip install python-dotenv
+```
+
 # Serverless Framework Python Flask API on AWS
 
 This template demonstrates how to develop and deploy a simple Python Flask API service running on AWS Lambda using the traditional Serverless Framework.
--- user-management/app.py
+++ user-management/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request, render_template
+from dotenv import load_dotenv, find_dotenv
+import os
+import boto3
 
 app = Flask(__name__)
+
+load_dotenv(find_dotenv())
+# read the .env-sample, to load the environment variable.
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env-var")
+load_dotenv(dotenv_path)
 
 
 @app.route("/")
@@ -11,6 +19,24 @@ def hello_from_root():
 @app.route("/hello")
 def hello():
     return jsonify(message='Hello from path!')
+
+
+@app.route("/signup", methods=['POST'])
+def signup_form():
+    # gets username and password from the form
+    username = request.form['username']
+    password = request.form['password']
+
+    client = boto3.client("cognito-idp", region_name="us-east-1")
+
+    # sign-up on Cognito
+    response = client.sign_up(
+        ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
+        Username=username,
+        Password=password,
+        UserAttributes=[{"Name": "email", "Value": username}],
+    )
+    return jsonify(message='Signed')
 
 
 @app.errorhandler(404)
