048c29f66c265c2520e3b9bc7f6dd721b62d3a7c
Jeisse Rocha <x21129924@student.ncirl.ie>
2022-03-29 10:00:49 +0100

    included signUp and auth

--- user-management/app.py
+++ user-management/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, make_response, request, render_template
 from dotenv import load_dotenv, find_dotenv
 import os
 import boto3
+import pprint
+from botocore.client import ClientError
 
 app = Flask(__name__)
 
@@ -21,22 +23,51 @@ def hello():
     return jsonify(message='Hello from path!')
 
 
-@app.route("/signup", methods=['POST'])
-def signup_form():
-    # gets username and password from the form
-    username = request.form['username']
-    password = request.form['password']
+@app.route("/signup")
+def sign_up():
+    username = request.args.get('username')
+    password = request.args.get('passwrd')
 
     client = boto3.client("cognito-idp", region_name="us-east-1")
 
-    # sign-up on Cognito
-    response = client.sign_up(
-        ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
-        Username=username,
-        Password=password,
-        UserAttributes=[{"Name": "email", "Value": username}],
-    )
-    return jsonify(message='Signed')
+    try:
+        # Add user to pool
+        sign_up_response = client.sign_up(
+            ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
+            Username=username,
+            Password=password,
+            UserAttributes=[{'Name': 'email',
+                             'Value': username}])
+        pprint(sign_up_response)
+        return jsonify(message="User successful created")
+    except ClientError as err:
+        # Probably user already exists
+        print(err)
+        return jsonify(message="error")
+
+
+@app.route("/auth")
+def init_auth():
+    username = request.args.get('username')
+    password = request.args.get('passwrd')
+
+    client = boto3.client("cognito-idp", region_name="us-east-1")
+
+    # This is less secure, but simpler
+    response = client.initiate_auth(
+        AuthFlow='USER_PASSWORD_AUTH',
+        AuthParameters={
+            'USERNAME': username,
+            'PASSWORD': password},
+        ClientId=os.getenv("COGNITO_USER_CLIENT_ID"))
+    print("----- Log in response -----")
+    pprint(response)
+    print("---------------------------")
+
+    # If authentication was successful we got three tokens
+    print(response['AuthenticationResult']['AccessToken'])
+    # response['AuthenticationResult']['IdToken']
+    # response['AuthenticationResult']['RefreshToken']
 
 
 @app.errorhandler(404)
